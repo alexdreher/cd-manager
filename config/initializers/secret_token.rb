b5c91726af5bbe6f359ef93ee90492cdb829d35a
Alexander Dreher <alexdreher@lxmedia.net>
2011-01-07 12:02:39 +0100

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CdManager::Application.config.secret_token = '117c24efc43a7802d043be4027dd1d0b9ba72112c1f329585dd61e2a187325c106bbfc18cec3d65bdc3b14460f4b254e4fd33ac54b2bafad93e77713827ece58'
