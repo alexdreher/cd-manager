5f210befe5f469aea9ddb7fca428dc7fe04d2c5f
Alexander Dreher <alexdreher@lxmedia.net>
2011-01-07 13:24:06 +0100

    implemented user authentication; Closes #1
    Squashed commit of the following:
    
    commit efdf9733a0552fe833b1789df5ff17fd90532168
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:22:23 2011 +0100
    
        green on sign out feature part
    
    commit 10cb9cd27a530201f8ae26b4c80cf14d170b5d8a
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:16:36 2011 +0100
    
        added sign out part to feature
    
    commit e8301142eac1608c415672d4a18f8eef60a561d5
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:14:40 2011 +0100
    
        green turned of capybara javascript emulation to get rid of this error:
        undefined local variable or method `node' for #<Capybara::Driver::RackTest::Node:0x1034bbd98> (NameError)
    
    commit 2e4df63fbf58bb99ca1c5dae11607b74c1b6c83e
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:13:18 2011 +0100
    
        red refactored feature definition to click links instead of browsing directly to sing (in/out)pages
    
    commit 1233f47999295fd71bd6924f745f025120bc37d3
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:08:10 2011 +0100
    
        green on the sign in feature; added sign in page path
    
    commit 45779e8ba81ade14ef032ea99301f7b2402e09e9
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:07:08 2011 +0100
    
        added db/test.sqlite3-journal to .gitignore
    
    commit 8dc49e84878fd81bd68ff860f5d946ef7e56a84a
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:05:42 2011 +0100
    
        red added given the following accounts step
    
    commit a8286b11e49586b5e91183881a845357c4b785c6
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 13:03:12 2011 +0100
    
        red added scenario outline sign in to authentication
    
    commit 22979ed8c72d046f7694d3866576ddd0a15cac6b
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:58:27 2011 +0100
    
        green on first part; damn typos
    
    commit 81e5a4eff926077d4448439b1df64fa1aa21d3b8
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:54:04 2011 +0100
    
        fixed type in feature
    
    commit 058969c083f4ce2f38682707ef2dbd16c28d6ec6
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:51:20 2011 +0100
    
        revert layout spec
    
    commit b7649c73b813f574c14cc9a8433e1991110e83d1
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:39:33 2011 +0100
    
        red authentication links in layout spec
    
    commit 0650ba58b15fe72ade8b78f8e5421d89cd26b5e6
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:32:11 2011 +0100
    
        added "the sign up page" path
    
    commit 2cf995529eba52218020c65b7d0de7c9a4a1ebe6
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:30:28 2011 +0100
    
        added step definition "not logged in"
    
    commit 80fc152182d46a5748464d7f4212e7ead99f4164
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:26:25 2011 +0100
    
        added devise user scaffold
    
    commit 66c20996582d2f296d4afb9a76365371b1951963
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:22:42 2011 +0100
    
        added devise fpr authentication
    
    commit f115ad05c304e257c72046d5ce5a24caac405b51
    Author: Alexander Dreher <alexdreher@lxmedia.net>
    Date:   Fri Jan 7 12:19:33 2011 +0100
    
        red added user authentication feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,12 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+      
+    when /the sign up page/
+      new_user_registration_path
+      
+    when /the sign in page/
+      new_user_session_path
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
